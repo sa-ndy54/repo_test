4e1bbfb70e4cb1576d1976785c2b50e34f3044bd
Sandeep Joshi <sandeep.joshi@newgensoft.com>
2025-08-07 15:20:42 +0530

    After long time

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "bcf304d8-21b8-4358-9c77-54b5dd26f938"
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "FirstRG" {
+  name     = "SJ_08"
+  location = "Central India"
+}
